727c74fa829d7773b290a9112a9c3a16358079c6
Manoj Kharkar <manojkharkar909@gmail.com>
2025-11-30 19:33:51 +0000

    adding liefle js and css

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const socketio = require("socket.io");
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.set("view engine", ejs);
-app.set(express.static(path.join(__dirname, "public")))
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/", function (req, res) {
-    res.send("hey");
+    res.render("index");
 });
 
 server.listen(3000);
