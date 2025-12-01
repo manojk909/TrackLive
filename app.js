e0ca75c6b05d278d060756ed9e06c1df8df71181
Manoj Kharkar <manojkharkar909@gmail.com>
2025-12-01 05:58:10 +0000

    integrating map

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const express = require("express");
 const app = express();
 
@@ -11,8 +12,12 @@ const io = socketio(server);
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")))
 
+io.on("connection", function (socket) {
+    console.log(("connected"));
+});
+
 app.get("/", function (req, res) {
     res.render("index");
 });
 
-server.listen(3000);
+server.listen(3000)
--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,24 @@
 const socket = io();
 
-console.log("heyy")
-console.log("hiii")
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitide, longitude } = position.coords;
+            socket.emit("send-location", { latitide, longitude });
+        },
+        (error) => {
+            console.error(error);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+        }
+    )
+}
+
+L.map("map").setView([0, 0], 10);
+
+L.tileLayer("https://{s}.tileopenstreetmao.org/{z}/{x}/{y}.png", {
+    attrbution: "OPenStreetMap"
+}).addTo(map)
